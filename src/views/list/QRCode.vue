e0b4d7e7853f46c786f217c3565ef8ba3bd27367
guoweikai <guoweikai@joyme.sg>
2023-08-22 15:17:55 +0800

    feat:build

--- src/views/list/QRCode.vue
+++
@@ -1,8 +0,0 @@
-<!--
- * @Author: your name
- * @Date: 2023-08-22 14:19:04
- * @LastEditTime: 2023-08-22 14:19:05
- * @LastEditors: bogon
- * @Description: In User Settings Edit
- * @FilePath: /vue3-admin-electron/src/views/list/QRCode.vue
--->
